6e3cbb9e2aff5b47487067450f67de9645e3ad54
Michaelpkr <michael.parker@ramseysolutions.com>
2022-06-07 18:02:56 -0500

    Implemented Game Logic

--- war.js
+++ war.js
@@ -2,7 +2,18 @@ document.querySelector("#createDeck").addEventListener("click", createDeck)
 document.querySelector("#drawTwoCards").addEventListener("click", drawTwoCards)
 
 let deckId = ""
-    
+
+fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    deckId = data.deck_id
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
+
+function createDeck(){
     fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then(res => res.json())
     .then(data => {
@@ -13,6 +24,8 @@ let deckId = ""
         console.log(`error ${err}`)
     });
 
+}
+
     function drawTwoCards(){
     
         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
@@ -21,9 +34,33 @@ let deckId = ""
             document.querySelector("#playerCard").src = data.cards[0].image
             document.querySelector("#computerCard").src = data.cards[1].image
             document.querySelector("#cardsRemaining").innerText = `Cards Remaining: ${data.remaining}`
+            let playerCardValue = convertsValueToNumber(data.cards[0].value)
+            let computerCardValue = convertsValueToNumber(data.cards[1].value)
+
+
+            if(playerCardValue > computerCardValue){
+                document.querySelector("#gameResult").innerText = "Player Wins!"
+            }else if(computerCardValue > playerCardValue){
+                document.querySelector("#gameResult").innerText = "Computer Wins!"
+            }else document.querySelector("#gameResult").innerText = "War!"
+
+
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
         
+        }
+
+
+        function convertsValueToNumber(value){
+            if(value === "ACE"){
+                return 14
+            }else if(value === "KING"){
+                return 13
+            }else if(value === "QUEEN"){
+                return 12
+            }else if (value === "JACK"){
+                return 11
+            }else return Number(value)
         }
